d8c87a234c3317770a9ed2517bb50c9d1a3b40a2
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-01-17 12:33:04 -0800

    Update shipbuilding.js

--- shipbuilding.js
+++ shipbuilding.js
@@ -1,1 +1,17 @@
-var Part = new Object(name, mass, hp, cost, size); 
+function Part(name, mass, hp, cost, size) {
+  this.name = name; 
+  this.mass = mass; 
+  this.hp = hp; 
+  this.cost = cost; 
+  this.size = size; 
+  this.XYPos = [null, null]; 
+  function this.getPartText() {
+    var raw_code = '{"pos":[' + String(this.XYPos[0]) + ',' + String(this.XYPos[1]) + '],"type":"' + this.name + '","dir":0}'; 
+    console.log(raw_code); 
+    return raw_code; 
+  } 
+  /*function this.getCoordsOccupied() {
+    var coords_occupied = [null, null]; 
+    var xy_coords = this.getXYPos(); 
+  */ 
+} 
