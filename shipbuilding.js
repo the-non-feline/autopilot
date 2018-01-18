92c3f167da7f077119427159ff01c02a1de40fea
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-01-18 12:44:20 -0800

    replaced reserved keywords

--- shipbuilding.js
+++ shipbuilding.js
@@ -1,22 +1,22 @@
-function Part(name, mass, hp, cost, size) {
-  this.name = name; 
+function Part(part_name, mass, hp, cost, area) {
+  this.part_name = part_name; 
   this.mass = mass; 
   this.hp = hp; 
   this.cost = cost; 
-  this.size = size; 
+  this.area = area; 
   this.XYPos = [null, null]; 
   function this.getPartText() {
-    var raw_code = '{"pos":[' + String(this.XYPos[0]) + ',' + String(this.XYPos[1]) + '],"type":"' + this.name + '","dir":0}'; 
+    var raw_code = '{"pos":[' + String(this.XYPos[0]) + ',' + String(this.XYPos[1]) + '],"type":"' + this.part_name + '","dir":0}'; 
     console.log(raw_code); 
     return raw_code; 
   } 
   function this.getCoordsOccupied() {
     var coords_occupied = []; 
     var xy_center = this.getXYPos(); 
-    for(var x = 0; x < this.size[0]; x++) {
-      for(var y = 0; y < this.size[1]; y++) {
-        var scaledX = xy_center[0] + x * 20 - this.size[0] * 10 + 10; 
-        var scaledY = xy_center[1] + y * 20 - this.size[1] * 10 + 10; 
+    for(var x = 0; x < this.area[0]; x++) {
+      for(var y = 0; y < this.area[1]; y++) {
+        var scaledX = xy_center[0] + x * 20 - this.area[0] * 10 + 10; 
+        var scaledY = xy_center[1] + y * 20 - this.area[1] * 10 + 10; 
         coords_occupied.append([scaledX, scaledY]) 
       } 
     } 
